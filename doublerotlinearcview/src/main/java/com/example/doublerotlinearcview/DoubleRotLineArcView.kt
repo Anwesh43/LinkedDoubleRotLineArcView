880013a43d99ef8205b056f6368293a03511c4be
Anwesh43 <anweshthecool0@gmail.com>
2020-12-17 04:50:47 +0530

    adding DRLANode class

--- doublerotlinearcview/src/main/java/com/example/doublerotlinearcview/DoubleRotLineArcView.kt
+++ doublerotlinearcview/src/main/java/com/example/doublerotlinearcview/DoubleRotLineArcView.kt
@@ -129,4 +129,45 @@ class DoubleRotLineArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DRLANode(var i : Int, val state : State = State()) {
+
+        private var next : DRLANode? = null
+        private var prev : DRLANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DRLANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DRLANode {
+            var curr : DRLANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
