14bb74d0af7e63be0c91382cb87e1273c1bb874f
Anwesh43 <anweshthecool0@gmail.com>
2020-12-17 04:41:36 +0530

    adding DoubleRotLineArcView class

--- doublerotlinearcview/src/main/java/com/example/doublerotlinearcview/DoubleRotLineArcView.kt
+++ doublerotlinearcview/src/main/java/com/example/doublerotlinearcview/DoubleRotLineArcView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawDRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawDoubleRotLineArc(scale, w, h, paint)
 }
+
+class DoubleRotLineArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
