e954220a66551d49467eb5d23b980db4a40a7c38
Anwesh43 <anweshthecool0@gmail.com>
2020-12-17 05:08:49 +0530

    adding fullscreen and view in activity

--- app/src/main/java/com/example/linkeddoublerotlinearcview/MainActivity.kt
+++ app/src/main/java/com/example/linkeddoublerotlinearcview/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.linkeddoublerotlinearcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.doublerotlinearcview.DoubleRotLineArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DoubleRotLineArcView.create(this)
+        fullScreen()
     }
 }
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
+
